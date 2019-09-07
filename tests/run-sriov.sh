ca7bffa52816a84195a2b20a3001911e6a42f1ad
Zenghui Shi <zshi@redhat.com>
2019-09-07 17:58:59 +0800

    tests: change KUBECONFIG path

--- tests/run-sriov.sh
+++ tests/run-sriov.sh
@@ -7,7 +7,7 @@ cd $WORKSPACE/origin
 make WHAT=cmd/openshift-tests
 
 export OPENSHIFT_TESTS=$WORKSPACE/origin/_output/local/bin/linux/amd64/openshift-tests
-export KUBECONFIG=$WORKSPACE/../OCP-Networking-Multus-Install-KNI-08/dev-scripts/ocp/auth/kubeconfig
+export KUBECONFIG=$WORKSPACE/../OCP-KNI-Install/dev-scripts/ocp/auth/kubeconfig
 
 $OPENSHIFT_TESTS run all --dry-run  | grep "sriov" | $OPENSHIFT_TESTS run -f -
 $OPENSHIFT_TESTS run all --dry-run  | grep "dpdk" | $OPENSHIFT_TESTS run -f -
