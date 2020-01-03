0e21e6ce3a4ca275f92c935d59cae68764edf07b
Zenghui Shi <zshi@redhat.com>
2020-01-03 19:00:26 +0800

    aggragate sriov tests scripts in run.sh

---
+++ tests/sriov/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+trap cleanup 0 1
+
+cleanup() {
+	oc get clusterversion || true
+	oc describe nodes || true
+	oc get nodes || true
+	oc get pods -n openshift-sriov-network-operator || true
+	oc get pods -n default || true
+	./cleanup.sh || true
+	popd
+}
+
+pushd openshift-baremetal-ci/tests/sriov
+
+wget http://lacrosse.corp.redhat.com/~zshi/ocp/image-references.sh
+source ./image-references.sh
+
+./run-sriov-operator.sh
+
+sleep 20
+
+./run-pod.sh
