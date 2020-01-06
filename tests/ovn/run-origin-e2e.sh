f12d1824f80fd36e4bbdda79782e491a963230c5
Zenghui Shi <zshi@redhat.com>
2020-01-06 14:11:21 +0800

    ignore Disabled origin e2e tests

--- tests/ovn/run-origin-e2e.sh
+++ tests/ovn/run-origin-e2e.sh
@@ -14,4 +14,5 @@ make WHAT=cmd/openshift-tests
 OPENSHIFT_TESTS=$(realpath ./_output/local/bin/linux/amd64/openshift-tests)
 
 # run sig-network tests
-$OPENSHIFT_TESTS run all --dry-run | grep -E "sig-network" | $OPENSHIFT_TESTS run -f -
+$OPENSHIFT_TESTS run all --dry-run | grep -E "sig-network" | \
+	grep -v "Disabled:" | $OPENSHIFT_TESTS run -f -
