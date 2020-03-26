6bb3022121486e2a39b25b753a537a05c7044d06
zshi-redhat <zshi@redhat.com>
2020-03-26 10:46:47 +0800

    update baremetal pipeline

--- pipeline/baremetal.groovy
+++ pipeline/baremetal.groovy
@@ -1,16 +1,12 @@
 #!/usr/bin/env groovy
 
 pipeline {
-	agent {
-		node {
-			label 'nfvpe-multus-05'
-		}
-	}
+	agent any
 	stages {
 		stage('OCP Multus') {
 			steps {
 				script {
-					def jobDeploy = build job: 'OVN-UPI-Install', wait: true, propagate: false
+					def jobDeploy = build job: 'OVN-UPI-Install-4.4', wait: true, propagate: false
 					def jobDeployResult = jobDeploy.getResult()
 
 					if (jobDeployResult == 'SUCCESS') {
@@ -19,30 +15,6 @@ pipeline {
 						build job: 'OCP-SRIOV-DEV', wait: true
 						build job: 'OCP-SRIOV-CONFORMANCE', wait: true
 						build job: 'OCP-SRIOV-OPERATOR-E2E', wait: true
-					}
-				}
-			}
-		}
-		stage('OCP RIPSAW OVN') {
-			steps {
-				script {
-					def jobDeploy = build job: 'OVN-UPI-Install', wait: true, propagate: false
-					def jobDeployResult = jobDeploy.getResult()
-
-					if (jobDeployResult == 'SUCCESS') {
-						build job: 'OCP-RIPSAW-OVN', wait: true
-					}
-				}
-			}
-		}
-		stage('OCP RIPSAW SRIOV') {
-			steps {
-				script {
-					def jobDeploy = build job: 'OVN-UPI-Install', wait: true, propagate: false
-					def jobDeployResult = jobDeploy.getResult()
-
-					if (jobDeployResult == 'SUCCESS') {
-						build job: 'OCP-RIPSAW-SRIOV', wait: true
 					}
 				}
 			}
