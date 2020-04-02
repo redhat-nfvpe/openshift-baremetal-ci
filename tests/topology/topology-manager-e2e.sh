484b80c2a1144d95a581445067965499d92a77b8
zshi-redhat <zshi@redhat.com>
2020-04-02 21:49:45 +0800

    add sriov config for TM e2e test

--- tests/topology/topology-manager-e2e.sh
+++ tests/topology/topology-manager-e2e.sh
@@ -19,6 +19,15 @@ make WHAT=cmd/openshift-tests
 
 OPENSHIFT_TESTS=$(realpath ./_output/local/bin/linux/amd64/openshift-tests)
 
+# create sriov network
+oc create -f templates/sn-intel.yaml
+
+# add SR-IOV related environment variables
+# https://github.com/fromanirh/origin/blob/topomgr-e2e-tests-ci/test/extended/topology_manager/README.md
+export SRIOV_NETWORK_NAMESPACE=default
+export SRIOV_NETWORK=sriov-intel
+export RESOURCE_NAME=openshift.io/intelnics
+
 $OPENSHIFT_TESTS run openshift/conformance --dry-run | \
 	grep -E "TopologyManager" | \
 	$OPENSHIFT_TESTS run -f -
