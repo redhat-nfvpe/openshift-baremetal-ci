498993530349e0d8babadccef3bb7db9928aa4e3
Francesco Romani <fromani@redhat.com>
2020-04-28 19:28:09 +0200

    topomgr: tests merged in the master branch
    the Topology Manager e2e tests have been merged in the
    openshift origin master branch: there is no more need
    to use fromani's fork
    
    CAUTION: caches (`origin` directory) need to be deleted before
    to merge this patch.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- tests/sriov/topology-manager-e2e.sh
+++ tests/sriov/topology-manager-e2e.sh
@@ -2,17 +2,10 @@
 set -x
 
 if [ ! -d "origin" ]; then
-	# up until the e2e tests are merged upstream, we need to use fromani's fork
-	git clone https://github.com/fromanirh/origin.git
+	git clone https://github.com/openshift/origin.git
 fi
 
 pushd origin
-# this branch contains the stable patches which will be part of PRs against
-# openshift/origin
-git checkout topomgr-e2e-tests-ci
-# up until all the tests are merged, we need to make sure we always pull
-# the latest fixes and tests.
-git pull --rebase
 
 # build 'openshift-tests' binary
 make WHAT=cmd/openshift-tests
