6f647a30e862eb1158a6813d89f64a815fdbdc4a
Zenghui Shi <zshi@redhat.com>
2021-11-11 12:26:52 +0800

    ovn egressip: fix egress-ip folder name

--- tests/offload/egress-ip/run.sh
+++ tests/offload/egress-ip/run.sh
@@ -2,14 +2,14 @@
 
 oc label nodes worker-advnetlab24 k8s.ovn.org/egress-assignable=""
 
-pushd openshift-baremetal-ci/tests/offload/egressip
+pushd openshift-baremetal-ci/tests/offload/egress-ip
 oc apply -f namespace.yaml
 oc apply -f egressip.yaml
 popd
 
 sleep 5
 
-pushd openshift-baremetal-ci/tests/offload/egressip
+pushd openshift-baremetal-ci/tests/offload/egress-ip
 oc apply -f pod-egressip.yaml
 popd
 
@@ -18,7 +18,7 @@ oc wait --for condition=ready pods testpod-egressip-worker-24 -n egressip --time
 
 oc exec testpod-egressip-worker-24 -- ping -c 10 redhat.com
 
-pushd openshift-baremetal-ci/tests/offload/egressip
+pushd openshift-baremetal-ci/tests/offload/egress-ip
 oc delete -f pod-egressip.yaml
 oc delete -f egressip.yaml
 oc delete -f namespace.yaml
