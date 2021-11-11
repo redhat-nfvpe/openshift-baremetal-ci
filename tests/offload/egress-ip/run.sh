92b0193499cbd93f8aca30b8999432c90ba9bfc1
Zenghui Shi <zshi@redhat.com>
2021-11-11 12:23:56 +0800

    add ovn egress ip test

---
+++ tests/offload/egress-ip/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+oc label nodes worker-advnetlab24 k8s.ovn.org/egress-assignable=""
+
+pushd openshift-baremetal-ci/tests/offload/egressip
+oc apply -f namespace.yaml
+oc apply -f egressip.yaml
+popd
+
+sleep 5
+
+pushd openshift-baremetal-ci/tests/offload/egressip
+oc apply -f pod-egressip.yaml
+popd
+
+sleep 2
+oc wait --for condition=ready pods testpod-egressip-worker-24 -n egressip --timeout=30
+
+oc exec testpod-egressip-worker-24 -- ping -c 10 redhat.com
+
+pushd openshift-baremetal-ci/tests/offload/egressip
+oc delete -f pod-egressip.yaml
+oc delete -f egressip.yaml
+oc delete -f namespace.yaml
+popd
+
+oc label nodes worker-advnetlab24 k8s.ovn.org/egress-assignable-
