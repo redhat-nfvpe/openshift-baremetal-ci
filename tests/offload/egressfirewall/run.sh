91bb9fe1f1fd46ce7335b14b640957a88ffa968f
Zenghui Shi <zshi@redhat.com>
2021-11-11 16:43:43 +0800

    ovn egressfirewall: run tests before and after applying egressfirewall rules

--- tests/offload/egressfirewall/run.sh
+++ tests/offload/egressfirewall/run.sh
@@ -15,10 +15,9 @@ cleanup() {
 
 pushd openshift-baremetal-ci/tests/offload/egressfirewall
 oc apply -f namespace.yaml
-oc apply -f egressfirewall.yaml
 popd
 
-sleep 5
+sleep 1
 
 pushd openshift-baremetal-ci/tests/offload/egressfirewall
 oc apply -f pod-egressfirewall.yaml
@@ -27,21 +26,45 @@ popd
 sleep 2
 oc wait --for condition=ready pods testpod-egressfirewall-worker-24 -n egressfirewall --timeout=30s
 
-ret=$(oc -n egressfirewall exec testpod-egressfirewall-worker-24 -- ping -c 10 google.com)
-
+set +e
+oc -n egressfirewall exec testpod-egressfirewall-worker-24 -- ping -c 10 google.com
 if [ $ret == 0 ]; then
-	echo "ping to google.com to succeed, expected"
+	echo "ping to google.com succeeded, expected"
 else
 	echo "expect ping to google.com to succeed, but failed"
 	exit 1
 fi
 
-ret=$(oc -n egressfirewall exec testpod-egressfirewall-worker-24 -- ping -c 10 redhat.com)
+oc -n egressfirewall exec testpod-egressfirewall-worker-24 -- ping -c 10 redhat.com
+if [ $ret == 0 ]; then
+	echo "ping to redhat.com succeeded, expected"
+else
+	echo "expect ping to redhat.com to succeed, but failed"
+	exit 1
+fi
+set -e
 
+pushd openshift-baremetal-ci/tests/offload/egressfirewall
+echo "apply egress firewall rules to deny access to redhat.com"
+oc apply -f egressfirewall.yaml
+popd
+
+sleep 5
+
+set +e
+oc -n egressfirewall exec testpod-egressfirewall-worker-24 -- ping -c 10 google.com
+if [ $ret == 0 ]; then
+	echo "ping to google.com succeeded, expected"
+else
+	echo "expect ping to google.com to succeed, but failed"
+	exit 1
+fi
+
+oc -n egressfirewall exec testpod-egressfirewall-worker-24 -- ping -c 10 redhat.com
 if [ $ret == 0 ]; then
 	echo "expect ping to redhat.com to fail, but succeeded"
 	exit 1
 else
-	echo "ping to redhat.com to fail, expected"
+	echo "ping to redhat.com failed, expected"
 fi
-
+set -e
