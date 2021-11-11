5bf39d4134254d1ab070cf4702543b51fc351bd1
Zenghui Shi <zshi@redhat.com>
2021-11-11 17:02:12 +0800

    ovn egressfirewall: update expected results

--- tests/offload/egressfirewall/run.sh
+++ tests/offload/egressfirewall/run.sh
@@ -45,7 +45,7 @@ fi
 set -e
 
 pushd openshift-baremetal-ci/tests/offload/egressfirewall
-echo "apply egress firewall rules to deny access to redhat.com"
+echo "applying egress firewall rules"
 oc apply -f egressfirewall.yaml
 popd
 
@@ -54,17 +54,17 @@ sleep 5
 set +e
 oc -n egressfirewall exec testpod-egressfirewall-worker-24 -- ping -c 10 google.com
 if [ $? == 0 ]; then
-	echo "ping to google.com succeeded, expected"
-else
-	echo "expect ping to google.com to succeed, but failed"
+	echo "expect ping to google.com to fail, but succeeded"
 	exit 1
+else
+	echo "ping to google.com failed, expected"
 fi
 
 oc -n egressfirewall exec testpod-egressfirewall-worker-24 -- ping -c 10 redhat.com
 if [ $? == 0 ]; then
-	echo "expect ping to redhat.com to fail, but succeeded"
-	exit 1
+	echo "ping to redhat.com succeeded, expected"
 else
-	echo "ping to redhat.com failed, expected"
+	echo "expect ping to redhat.com to succeed, but failed"
+	exit 1
 fi
 set -e
