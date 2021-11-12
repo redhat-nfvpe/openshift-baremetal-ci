33c5171f8c48af951119e4ec7eb205cafd18b06b
Zenghui Shi <zshi@redhat.com>
2021-11-12 16:04:55 +0800

    ovn egressrouter: update egressrouter test

--- tests/offload/egressrouter/run.sh
+++ tests/offload/egressrouter/run.sh
@@ -29,14 +29,55 @@ popd
 sleep 2
 oc wait --for condition=ready pods testpod-egressrouter-worker-24 -n egressrouter --timeout=30s
 
-svc=$(oc get svc egressrouter-svc -ojsonpath='{.spec.clusterIP}')
+svc=$(oc -n egressrouter get svc egressrouter-svc -ojsonpath='{.spec.clusterIP}')
+podip=$(oc get pods -l app=egress-router-cni -n egressrouter -ojsonpath='{.items[0].status.podIP}')
 
 set +e
 oc -n egressrouter exec testpod-egressrouter-worker-24 -- curl $svc:80
 if [ $? == 0 ]; then
-	echo "curl to $svc:80 succeeded, expected"
+	echo "curl to service $svc:80 succeeded, expected"
 else
-	echo "expect curl to $svc:80 to succeed, but failed"
+	echo "expect curl to service $svc:80 to succeed, but failed"
+	exit 1
+fi
+
+oc -n egressrouter exec testpod-egressrouter-worker-24 -- curl $svc:5000
+if [ $? == 0 ]; then
+	echo "curl to service $svc:5000 succeeded, expected"
+else
+	echo "expect curl to service $svc:5000 to succeed, but failed"
+	exit 1
+fi
+
+oc -n egressrouter exec testpod-egressrouter-worker-24 -- curl $svc:6000
+if [ $? == 0 ]; then
+	echo "curl to service $svc:6000 succeeded, expected"
+else
+	echo "expect curl to service $svc:6000 to succeed, but failed"
+	exit 1
+fi
+
+oc -n egressrouter exec testpod-egressrouter-worker-24 -- curl $svc:80
+if [ $? == 0 ]; then
+	echo "curl to endpoint pod $podip:80 succeeded, expected"
+else
+	echo "expect curl to endpoint pod $podip:80 to succeed, but failed"
+	exit 1
+fi
+
+oc -n egressrouter exec testpod-egressrouter-worker-24 -- curl $svc:8080
+if [ $? == 0 ]; then
+	echo "curl to endpoint pod $podip:8080 succeeded, expected"
+else
+	echo "expect curl to endpoint pod $podip:8080 to succeed, but failed"
+	exit 1
+fi
+
+oc -n egressrouter exec testpod-egressrouter-worker-24 -- curl $svc:8888
+if [ $? == 0 ]; then
+	echo "curl to endpoint pod $podip:8888 succeeded, expected"
+else
+	echo "expect curl to endpoint pod $podip:8888 to succeed, but failed"
 	exit 1
 fi
 set -e
